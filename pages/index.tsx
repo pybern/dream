612da0932a899fad5149feced3426dd3ce4a450f
Bernard Lee <bernard.hex@gmail.com>
2022-08-31 08:24:45 +0000

    edited template

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Header from '../components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Build it!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+  
+      <Header /> 
+      {/* Banner */}
+
+    </div>
+  );
+}
+
+export default Home
