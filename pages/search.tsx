3b974cdfcc3e7f43fd160de8253f40298c372ae9
Bernard Lee <bernard.hex@gmail.com>
2022-09-14 04:39:55 +0000

    update

--- pages/search.tsx
+++ pages/search.tsx
@@ -13,13 +13,14 @@ function Search() {
 
     const formattedStartDate = format(new Date(startDate), "dd MMMM yy")
     const formattedEndDate = format(new Date(endDate), "dd MMMM yy")
+    const dateRange = `${formattedStartDate} to ${formattedEndDate}`
 
   return (
     <div>     
         <Header />
         <main className="flex">
             <section className="flex-grow pt-14 px-6">
-                <p className="text-xs">Booking results for {noOfGuest} vehicles from {formattedStartDate} to {formattedEndDate}</p>
+                <p className="text-xs">Booking results for {noOfGuest} vehicles from {dateRange}</p>
                 <h1 className="text-3xl font-semibold mt-2 mb-6">For {location}</h1>
                 <div className="hidden md:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
                     <p className="button">Cancellation Flexibility</p>
